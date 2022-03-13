9c5dc5554aa27d36a9b8670a91435f1b3e869285
Tanner S <smithjtanner@gmail.com>
2022-03-13 01:21:47 -0700

    Added c- for html (not working)

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is now active!');
+	console.log('Congratulations, Cydran IntelliSense is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -42,34 +42,6 @@ export function activate(context: vscode.ExtensionContext) {
 		'.' // triggered whenever a '.' is being typed
 	);
 
-	// const pubSub = vscode.languages.registerCompletionItemProvider(
-	// 	'javascript',
-	// 	{
-	// 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-
-	// 				const message = new vscode.CompletionItem('this.message');
-	// 				// ${1|this|or|this|}, ${2:SampleText}
-	// 				message.insertText = new vscode.SnippetString('this.message(\'${1:channelName}\', \'${2:messageType}\', ${3:somePayLoad});');
-	// 				const messageDocs : any = new vscode.MarkdownString("publish a message directly.");
-	// 				message.documentation = messageDocs;
-
-	// 				//this is for adding docs linking which we cant do at work anyway :(
-	// 				// docs.baseUri = vscode.Uri.parse('http://example.com/a/b/c/');
-
-	// 				const broadcast = new vscode.CompletionItem('this.broadcast');
-	// 				broadcast.insertText = new vscode.SnippetString('this.broadcast(\'${1:channelName}\', \'${2:messageType}\', ${3:somePayLoad});');
-	// 				const broadcastDocs : any = new vscode.MarkdownString("broadcast a message.");
-	// 				broadcast.documentation = broadcastDocs;
-
-
-	// 			return [
-	// 				message,
-	// 				broadcast,
-	// 				new vscode.CompletionItem('this.globalBrodcast', vscode.CompletionItemKind.Method)
-	// 			];
-	// 		}
-	// 	});
-
 	const pubSub = vscode.languages.registerCompletionItemProvider(
 		'javascript',
 		{
@@ -78,28 +50,82 @@ export function activate(context: vscode.ExtensionContext) {
 				const message = new vscode.CompletionItem('message', vscode.CompletionItemKind.Method);
 				// ${1|this|or|this|}, ${2:SampleText}
 				message.insertText = new vscode.SnippetString('message(\'${1:channelName}\', \'${2:messageType}\', ${3:somePayLoad});');
-				const messageDocs: any = new vscode.MarkdownString("publish a message directly.");
+				const messageDocs: any = new vscode.MarkdownString("Cydran: publish a message directly");
 				message.documentation = messageDocs;
 
+				const broadcast = new vscode.CompletionItem('broadcast', vscode.CompletionItemKind.Method);
+				broadcast.insertText = new vscode.SnippetString('broadcast(\'${1:channelName}\', \'${2:messageType}\', ${3:somePayLoad});');
+				const broadcastDocs: any = new vscode.MarkdownString("Cydran: broadcast a message");
+				broadcast.documentation = broadcastDocs;
+
+				const broadcastGlobal = new vscode.CompletionItem('broadcastGlobally', vscode.CompletionItemKind.Method);
+				broadcastGlobal.insertText = new vscode.SnippetString('broadcast(\'${1:channelName}\', \'${2:messageType}\', ${3:somePayLoad});');
+				const broadcastGlobalDocs: any = new vscode.MarkdownString("Cydran: broadcast a message in the Cydran global scope");
+				broadcastGlobal.documentation = broadcastGlobalDocs;
+
+				const directMessage = new vscode.CompletionItem('on', vscode.CompletionItemKind.Method);
+				directMessage.insertText = new vscode.SnippetString('on(\"${1:messageType}\").invoke(${2:expression});');
+				const directMessageDocs: any = new vscode.MarkdownString("Cydran: Listening for direct messages");
+				directMessage.documentation = directMessageDocs;
+
+				const broadcastMessage = new vscode.CompletionItem('on', vscode.CompletionItemKind.Method);
+				broadcastMessage.insertText = new vscode.SnippetString('on(\"${1:messageType}\").forChannel(\"${2:channelName}\").invoke(${3:expression});');
+				const broadcastMessageDocs: any = new vscode.MarkdownString("Cydran: listening for brodcast messages");
+				broadcastMessage.documentation = broadcastMessageDocs;
 
 
 				// get all text until the `position` and check if it reads `console.`
 				// and if so then complete if `log`, `warn`, and `error`
 				const linePrefix = document.lineAt(position).text.substring(0, position.character);
 				if (!linePrefix.endsWith('this.')) {
+					console.log('pfx: ' + linePrefix);
 					return undefined;
 				}
 
+				console.log('pfx: match');
 				return [
-					new vscode.CompletionItem('broadcast', vscode.CompletionItemKind.Method),
-					new vscode.CompletionItem('globalBroadcast', vscode.CompletionItemKind.Method),
-					message
+					broadcast,
+					broadcastGlobal,
+					message,
+					directMessage,
+					broadcastMessage
 				];
 			}
 		}, '.'
 	);
 
+	const htmlCommands = vscode.languages.registerCompletionItemProvider(
+		'html',
+		{
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 
+				const ceach = new vscode.CompletionItem('each', vscode.CompletionItemKind.Value);
+				ceach.insertText = new vscode.SnippetString('each=\'${1:List}\'');
+				const ceachDocs: any = new vscode.MarkdownString("Cydran: Repeating Cydran stuctures (arrays). Works similarly to a foreach loop. requires a c-each-mode=");
+				ceach.documentation = ceachDocs;
+
+				const ceachMode = new vscode.CompletionItem('each-mode', vscode.CompletionItemKind.Value);
+				ceachMode.insertText = new vscode.SnippetString('each-mode=\'${1|none|generated|expression|}\'');
+				const ceachModeDocs: any = new vscode.MarkdownString("Cydran: Repeating Cydran stuctures (arrays). Works similarly to a foreach loop. requires a c-each-mode=");
+				ceachMode.documentation = ceachModeDocs;
+
+
+
+				const linePrefix = document.lineAt(position).text.substring(0, position.character);
+				if (!linePrefix.endsWith('c-')) {
+					console.log('pfx: ' + linePrefix);
+					return undefined;
+				}
+
+				console.log('pfx: match');
+				//temp return
+				return [
+					ceach,
+					ceachMode
+				];
+			}
+		}, '-'
+	);
 
 	context.subscriptions.push(disposable, provider2, pubSub);
 }
