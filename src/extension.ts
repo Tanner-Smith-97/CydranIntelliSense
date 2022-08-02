c8ceee42f9e59863e1345ce67b07cb607f0ebb14
Tanner Smith <smithjtanner@gmail.com>
2022-08-01 20:50:48 -0600

    HTML Example working

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,36 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { readFileSync, existsSync } from 'fs';
+
+type CydranJson = {
+	cydran: [
+		{
+			completionItem: string,
+			snippetString: string,
+			markdownString: string
+		}
+	]
+};
+
+console.log(process.cwd());
+
+// const wf = vscode.workspace.workspaceFolders![0].uri.path;
+
+let cydranJson: CydranJson = onLoad();
+
+function onLoad(): CydranJson {
+	console.log('in onLoad');
+	if (existsSync('./cydran.json')) {
+		console.log('debug json loaded');
+		return JSON.parse(readFileSync('./cydran.json', 'utf-8'));
+	} else if (existsSync('./resources/app/extensions/cydran-intellisense/cydran.json')) {
+		console.log('prod json loaded');
+		return JSON.parse(readFileSync('./resources/app/extensions/cydran-intellisense/cydran.json', 'utf-8'));
+	} else {
+		throw new Error('It didn\'t work :(');
+	}
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,124 +46,56 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('cydran-intellisense.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello Cydran from Cydran IntelliSense!');
+		vscode.window.showInformationMessage('Hello from Cydran IntelliSense!');
 	});
-
-	//for debugging
-	const provider2 = vscode.languages.registerCompletionItemProvider(
-		'plaintext',
-		{
-			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-
-				// get all text until the `position` and check if it reads `console.`
-				// and if so then complete if `log`, `warn`, and `error`
-				const linePrefix = document.lineAt(position).text.substring(0, position.character);
-				if (!linePrefix.endsWith('console.')) {
-					return undefined;
-				}
-
-				return [
-					new vscode.CompletionItem('log', vscode.CompletionItemKind.Method),
-					new vscode.CompletionItem('warn', vscode.CompletionItemKind.Method),
-					new vscode.CompletionItem('error', vscode.CompletionItemKind.Method),
-				];
-			}
-		},
-		'.' // triggered whenever a '.' is being typed
-	);
-
-	const pubSub = vscode.languages.registerCompletionItemProvider(
-		'javascript',
-		{
-			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-
-				const message = new vscode.CompletionItem('message', vscode.CompletionItemKind.Method);
-				// ${1|this,or,this|}, ${2:SampleText}
-				message.insertText = new vscode.SnippetString('message(\'${1:channelName}\', \'${2:messageType}\', ${3:somePayLoad});');
-				const messageDocs: any = new vscode.MarkdownString("Cydran: publish a message directly");
-				message.documentation = messageDocs;
-
-				const broadcast = new vscode.CompletionItem('broadcast', vscode.CompletionItemKind.Method);
-				broadcast.insertText = new vscode.SnippetString('broadcast(\'${1:channelName}\', \'${2:messageType}\', ${3:somePayLoad});');
-				const broadcastDocs: any = new vscode.MarkdownString("Cydran: broadcast a message");
-				broadcast.documentation = broadcastDocs;
-
-				const broadcastGlobal = new vscode.CompletionItem('broadcastGlobally', vscode.CompletionItemKind.Method);
-				broadcastGlobal.insertText = new vscode.SnippetString('broadcast(\'${1:channelName}\', \'${2:messageType}\', ${3:somePayLoad});');
-				const broadcastGlobalDocs: any = new vscode.MarkdownString("Cydran: broadcast a message in the Cydran global scope");
-				broadcastGlobal.documentation = broadcastGlobalDocs;
-
-				const directMessage = new vscode.CompletionItem('on', vscode.CompletionItemKind.Method);
-				directMessage.insertText = new vscode.SnippetString('on(\"${1:messageType}\").invoke(${2:expression});');
-				const directMessageDocs: any = new vscode.MarkdownString("Cydran: Listening for direct messages");
-				directMessage.documentation = directMessageDocs;
-
-				const broadcastMessage = new vscode.CompletionItem('on', vscode.CompletionItemKind.Method);
-				broadcastMessage.insertText = new vscode.SnippetString('on(\"${1:messageType}\").forChannel(\"${2:channelName}\").invoke(${3:expression});');
-				const broadcastMessageDocs: any = new vscode.MarkdownString("Cydran: listening for brodcast messages");
-				broadcastMessage.documentation = broadcastMessageDocs;
-
-
-				// get all text until the `position` and check if it reads `console.`
-				// and if so then complete if `log`, `warn`, and `error`
-				const linePrefix = document.lineAt(position).text.substring(0, position.character);
-				if (!linePrefix.endsWith('this.')) {
-					console.log('pfx: ' + linePrefix);
-					return undefined;
-				}
-
-				console.log('pfx: match');
-				return [
-					broadcast,
-					broadcastGlobal,
-					message,
-					directMessage,
-					broadcastMessage
-				];
-			}
-		}, '.'
-	);
 
 	const htmlCommands = vscode.languages.registerCompletionItemProvider(
 		'html',
 		{
-			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
 
-				const ceach = new vscode.CompletionItem('each');
-				ceach.insertText = new vscode.SnippetString('each=\'${1:List}\'');
-				const ceachDocs: any = new vscode.MarkdownString("Cydran: Repeating Cydran stuctures (arrays). Works similarly to a foreach loop. requires a c-each-mode=");
-				ceach.documentation = ceachDocs;
+				const cydranCompletionItems: vscode.CompletionItem[] = [];
 
-				const ceachMode = new vscode.CompletionItem('each-mode');
-				ceachMode.insertText = new vscode.SnippetString('each-mode=\'${1|none,generated,expression|}\'');
-				const ceachModeDocs: any = new vscode.MarkdownString("Cydran: Repeating Cydran stuctures (arrays). Works similarly to a foreach loop. requires a c-each-mode=");
-				ceachMode.documentation = ceachModeDocs;
+				cydranJson.cydran.forEach(cydranItem => {
+					const item = new vscode.CompletionItem(cydranItem.completionItem);
+					item.insertText = new vscode.SnippetString(cydranItem.snippetString);
+					const itemDocs = new vscode.MarkdownString(cydranItem.markdownString);
+					item.documentation = itemDocs;
+					cydranCompletionItems.push(item);
+				});
 
+				// const ceach = new vscode.CompletionItem('c-each');
+				// ceach.insertText = new vscode.SnippetString('c-each=\'${1:List}\'');
+				// const ceachDocs: any = new vscode.MarkdownString("Cydran: Repeating Cydran stuctures (arrays). Works similarly to a foreach loop. requires a c-each-mode=");
+				// ceach.documentation = ceachDocs;
 
-				const reger = /([\w\[\]]+\-[\w\[\]\.]*)/g;
+				// const ceachMode = new vscode.CompletionItem('c-each-mode');
+				// ceachMode.insertText = new vscode.SnippetString('c-each-mode=\'${1|none,generated,expression|}\'');
+				// const ceachModeDocs: any = new vscode.MarkdownString("Cydran: Repeating Cydran stuctures (arrays). This is c-each-mode");
+				// ceachMode.documentation = ceachModeDocs;
 
-				const linePrefix = document.lineAt(position).text.substring(0, position.character);
-				// if (!linePrefix.endsWith('c.')) {
-				// 	console.log('pfx: ' + linePrefix);
-				// 	return undefined;
-				// }
-				if (!linePrefix.match(reger)) {
-					console.log('pfx: ' + linePrefix);
-					return undefined;
-				}
-
-
-				console.log('pfx: match');
-				//temp return
-				return [
-					ceach,
-					ceachMode
-				];
+				return cydranCompletionItems;
 			}
-		}, '-'
+		}
 	);
 
-	context.subscriptions.push(disposable, provider2, pubSub);
+	context.subscriptions.push(htmlCommands, disposable);
+
+	// context.subscriptions.push(disposable, provider2, pubSub, htmlCommands);
+
+	// vscode.workspace.onDidChangeTextDocument(event => {
+	// 	insertAutoCloseTag(event);
+	// });
+
+	// let closeTag = vscode.commands.registerCommand('auto-close-tag.closeTag', () => {
+	// 	insertCloseTag();
+	// });
+
+	// let showStuff = 
+
+
+	// context.subscriptions.push(closeTag);
+
 }
 
 // this method is called when your extension is deactivated
